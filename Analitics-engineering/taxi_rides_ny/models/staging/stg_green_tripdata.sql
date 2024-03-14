5aa07328862429abbdaa815f469014dc66a51261
Anass Mouzoun <anassichigo12@gmail.com>
2024-03-14 16:28:11 +0000

    commit

--- Analitics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ Analitics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -3,6 +3,7 @@
         materialized='view'
     )
 }}
+
 with tripdata as 
 (
   select *,
@@ -41,11 +42,3 @@ select
     {{ get_payment_type_description("payment_type") }} as payment_type_description
 from tripdata
 where rn = 1
-
-
-dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
- {% if var('is_test_run', default=false) %}
-
-  limit 100
-
-{% endif %}
